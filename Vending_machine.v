ab3dd28866b609b8ff8b862af45187ad5e79a36e
Yatish-4257 <2023eeb1428@iitrpr.ac.in>
2026-01-13 13:34:10 +0530

    Update Vending_machine.v

--- Vending_machine.v
+++ Vending_machine.v
@@ -1,88 +1,74 @@
 module ven_machine(
     input clk, 
     input rst,
-    input [1:0]in,
+    input [1:0] in,
     output reg out,
-    output reg [1:0]change
-    );
-
-parameter s0 = 2'b00;
-parameter s1 = 2'b01;
-parameter s2 = 2'b10;
-
-reg[1:0] c_state,n_state;
-
-always @(posedge clk)
-     begin
-       if (rst == 1)
-         begin
-           c_state = 0;
-           n_state = 0;
-           change = 2'b00;
-         end
-       else
-        c_state = n_state;
-
-       case(c_state)
-         s0  :
-         if(in == 0)
-            begin
-               n_state = s0;
-               out = 0;
-               change = 2'b00;
+    output reg [1:0] change
+);
+    parameter s0 = 2'b00;
+    parameter s1 = 2'b01;
+    parameter s2 = 2'b10;
+    
+    reg [1:0] c_state, n_state;
+    
+    always @(posedge clk) begin
+        if (rst)
+            c_state <= s0;
+        else
+            c_state <= n_state;
+    end
+    
+    always @(*) begin
+        n_state = c_state;
+        out = 0;
+        change = 2'b00;
+        
+        case (c_state)
+            s0: begin
+                if (in == 2'b00) begin
+                    n_state = s0;
+                end
+                else if (in == 2'b01) begin
+                    n_state = s1;
+                end    
+                else if (in == 2'b10) begin
+                    n_state = s2;
+                end  
             end
-         else if(in == 2'b01)
-            begin
-                n_state = s1;
-                out = 0;
-                change = 2'b00;
-            end    
-         else if(in == 2'b10)
-            begin
-                n_state = s2;
-                out = 0;
-                change = 2'b00;
-            end  
-
-         s1  :
-         if(in == 0)
-            begin
-               n_state = s0;
-               out = 0;
-               change = 2'b01;
+            
+            s1: begin
+                if (in == 2'b00) begin
+                    n_state = s0;
+                    change = 2'b01;
+                end
+                else if (in == 2'b01) begin
+                    n_state = s2;
+                end    
+                else if (in == 2'b10) begin
+                    n_state = s0;
+                    out = 1;
+                end 
             end
-         else if(in == 2'b01)
-            begin
-                n_state = s2;
-                out = 0;
-                change = 2'b00;
-            end    
-         else if(in == 2'b10)
-            begin
-                n_state = s0;
-                out = 1;
-                change = 2'b00;
-            end 
-
-         s2  :
-         if(in == 0)
-            begin
-               n_state = s0;
-               out = 0;
-               change = 2'b10;
+            
+            s2: begin
+                if (in == 2'b00) begin
+                    n_state = s0;
+                    change = 2'b10;
+                end
+                else if (in == 2'b01) begin
+                    n_state = s0;
+                    out = 1;
+                end    
+                else if (in == 2'b10) begin
+                    n_state = s0;
+                    out = 1;
+                    change = 2'b01;
+                end
             end
-         else if(in == 2'b01)
-            begin
+            
+            default: begin
                 n_state = s0;
-                out = 1;
-                change = 2'b00;
-            end    
-         else if(in == 2'b10)
-            begin
-                n_state = s0;
-                out = 1;
-                change = 2'b01;
             end
         endcase
     end
-endmodule           
+endmodule
