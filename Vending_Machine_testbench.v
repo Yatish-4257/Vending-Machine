7e2b9e3fec2cd8de97af4f7c73b7ed2b40876305
Yatish-4257 <2023eeb1428@iitrpr.ac.in>
2026-01-13 13:37:08 +0530

    Update Vending_Machine_testbench.v

--- Vending_Machine_testbench.v
+++ Vending_Machine_testbench.v
@@ -1,27 +1,32 @@
 `include "ven_machine.v"
 module ven_machine_test;
-
-reg clk;
-reg [1:0]in;
-reg rst;
-
-wire out;
-wire [1:0]change;
-
-ven_machine uut(.clk(clk), .rst(rst), .in(in), .out(out), .change(change));
-
-initial begin
-    $dumpfile("ven_machine.vcd");
-    $dumpvars(0, ven_machine_test);
-    rst = 1;
-    clk = 0;
-
-    #6 rst = 0; in = 1;
-    #11 in = 1;
-    #16 in = 1;
-    #25 $finish;
+    reg clk;
+    reg [1:0] in;
+    reg rst;
+    wire out;
+    wire [1:0] change;
+    
+    ven_machine uut(.clk(clk), .rst(rst), .in(in), .out(out), .change(change));
+    
+    initial begin
+        $dumpfile("ven_machine.vcd");
+        $dumpvars(0, ven_machine_test);
+        $monitor("Time=%0t rst=%b in=%b state=%b out=%b change=%b", 
+                 $time, rst, in, uut.c_state, out, change);
+        
+        clk = 0;
+        rst = 1;
+        in = 0;
+        
+        #10 rst = 0;
+        #10 in = 2'b01;
+        #10 in = 2'b01;
+        #10 in = 2'b01;
+        #10 in = 2'b10;
+        #10 in = 2'b10;
+        #10 in = 2'b00;
+        #20 $finish;
     end
-
+    
     always #5 clk = ~clk;
-
 endmodule
