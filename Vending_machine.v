8e4c959b678f077bf13b1608cd03b558efd63b1f
Yatish-4257 <2023eeb1428@iitrpr.ac.in>
2026-01-14 13:56:38 +0530

    Update Vending_machine.v

--- Vending_machine.v
+++ Vending_machine.v
@@ -5,81 +5,81 @@ module ven_machine(
     output reg out,
     output reg [1:0] change
 );
+
     parameter s0 = 2'b00;
     parameter s1 = 2'b01;
     parameter s2 = 2'b10;
-    
+
     reg [1:0] c_state, n_state;
-    
-    always @(posedge clk) begin
-        if (rst)
-            c_state <= s0;
-        else
-            c_state <= n_state;
-    end
-    
+
+    // Combinational next-state + next-output logic
+    reg out_n;
+    reg [1:0] change_n;
+
     always @(*) begin
-        n_state = c_state;
-        out = 0;
-        change = 2'b00;
-        
+        // defaults
+        n_state   = c_state;
+        out_n     = 0;
+        change_n  = 2'b00;
+
         case (c_state)
             s0: begin
-                if (in == 2'b00) begin
-                    n_state = s0;
-                end
-                else if (in == 2'b01) begin
-                    n_state = s1;
-                end    
-                else if (in == 2'b10) begin
-                    n_state = s2;
-                end  
-                else begin
-                    n_state = s0;
-                end
+                if (in == 2'b00)       n_state = s0;
+                else if (in == 2'b01)  n_state = s1;
+                else if (in == 2'b10)  n_state = s2;
+                else                   n_state = s0;
             end
-            
+
             s1: begin
                 if (in == 2'b00) begin
-                    n_state = s0;
-                    change = 2'b01;
+                    n_state  = s0;
+                    change_n = 2'b01;
                 end
-                else if (in == 2'b01) begin
-                    n_state = s2;
-                end    
+                else if (in == 2'b01)  n_state = s2;
                 else if (in == 2'b10) begin
                     n_state = s0;
-                    out = 1;
-                end 
-                else begin
-                    n_state = s1;
+                    out_n   = 1;
                 end
+                else n_state = s1;
             end
-            
+
             s2: begin
                 if (in == 2'b00) begin
-                    n_state = s0;
-                    change = 2'b10;
+                    n_state  = s0;
+                    change_n = 2'b10;
                 end
                 else if (in == 2'b01) begin
                     n_state = s0;
-                    out = 1;
-                end    
+                    out_n   = 1;
+                end
                 else if (in == 2'b10) begin
-                    n_state = s0;
-                    out = 1;
-                    change = 2'b01;
+                    n_state  = s0;
+                    out_n    = 1;
+                    change_n = 2'b01;
                 end
-                else begin
-                    n_state = s2;
-                end
+                else n_state = s2;
             end
-            
+
             default: begin
-                n_state = s0;
-                out     = 0;
-                change  = 2'b00;
+                n_state   = s0;
+                out_n     = 0;
+                change_n  = 2'b00;
             end
         endcase
     end
+
+    // Sequential block (synchronous reset)
+    always @(posedge clk) begin
+        if (rst) begin
+            c_state <= s0;
+            out     <= 0;
+            change  <= 2'b00;
+        end
+        else begin
+            c_state <= n_state;
+            out     <= out_n;
+            change  <= change_n;
+        end
+    end
+
 endmodule
